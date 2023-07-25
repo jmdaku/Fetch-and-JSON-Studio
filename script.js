85718b8be069a6bd946b2af07c14e1149f88c93f
jmdaku <jmdaku@gmail.com>
2023-07-24 20:42:27 -0400

    started script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+
+
+window.addEventListener('load', function() {
+    const container = document.getElementById("container");
+    async function getAstronauts(){
+       let response = await fetch("https://handlers.education.launchcode.org/static/astronauts.json"); 
+    
+    let data = await response.json();
+    
+    console.log(data)
+
+    for(let i = 0; i < data.length; i++){
+        container.innerHTML += `
+        <div class="astronaut">
+        <div class="bio">
+        <h3>${data[i].firstName}  ${data[i].lastName}</h3>
+        <ul>
+        <li>Hours in space: ${data[i].hoursInSpace}</li>
+        <li>Active: ${data[i].active}</li>
+        <li>Skills: ${data[i].skills} </li>
+        </ul>
+        </div>
+        <img class="avatar" src="${data[i].picture}">
+        </div>
+        `
+       }
+    }      
+    getAstronauts();
+});
