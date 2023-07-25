28a3861895e12abadf988a47c786ba4d64cdec26
jmdaku <jmdaku@gmail.com>
2023-07-24 21:37:13 -0400

    added # of astronauts

--- script.js
+++ script.js
@@ -10,21 +10,22 @@ window.addEventListener('load', function() {
     
     console.log(data)
 
-    for(let i = 0; i < data.length; i++){
-        container.innerHTML += `
-        <div class="astronaut">
-        <div class="bio">
-        <h3>${data[i].firstName}  ${data[i].lastName}</h3>
-        <ul>
-        <li>Hours in space: ${data[i].hoursInSpace}</li>
-        <li>Active: ${data[i].active}</li>
-        <li>Skills: ${data[i].skills} </li>
-        </ul>
-        </div>
-        <img class="avatar" src="${data[i].picture}">
-        </div>
-        `
-       }
+         for(let i = 0; i < data.length; i++){
+            container.innerHTML += `
+            <div class="astronaut">
+            <div class="bio">
+            <h3>${data[i].firstName}  ${data[i].lastName}</h3>
+            <ul>
+            <li>Hours in space: ${data[i].hoursInSpace}</li>
+            <li>Active: ${data[i].active}</li>
+            <li>Skills: ${data[i].skills} </li>
+            </ul>
+            </div>
+            <img class="avatar" src="${data[i].picture}">
+            </div>
+            `
+        }
+        container.innerHTML += `Total Number of Astronauts: ${data.length}` 
     }      
     getAstronauts();
 });
